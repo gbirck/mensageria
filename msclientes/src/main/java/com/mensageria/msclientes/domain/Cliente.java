23932ae97afed0e1b5468367c69c55ba0127c562
Gabriel Birck <birckzin@gmail.com>
2024-12-06 17:35:03 -0300

    Pequenos ajustes

--- msclientes/src/main/java/com/mensageria/msclientes/domain/Cliente.java
+++ msclientes/src/main/java/com/mensageria/msclientes/domain/Cliente.java
@@ -17,11 +17,43 @@ public class Cliente {
     @Column(nullable = false)
     private String nome;
     @Column(nullable = false)
-    private int idade;
+    private Integer idade;
 
-    public Cliente(String cpf, String nome, int idade) {
+    public Cliente(String cpf, String nome, Integer idade) {
         this.cpf = cpf;
         this.nome = nome;
+        this.idade = idade;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Integer getIdade() {
+        return idade;
+    }
+
+    public void setIdade(Integer idade) {
         this.idade = idade;
     }
 }
